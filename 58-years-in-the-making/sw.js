2db8b68eda455436d0de040bb4dbc3dc56f97d58
qwertyKidsDC <qwertykidsdc@gmail.com>
2026-09-25 13:12:22 +1000

    Refresh Shelly keepsake cache

--- 58-years-in-the-making/sw.js
+++ 58-years-in-the-making/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shelly-65-github-v1";
+const CACHE = "shelly-65-github-v2";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
